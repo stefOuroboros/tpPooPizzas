aaee3ccec2d707253c9576a4ec643862949af066
Stéphane Gallois <stephane.gallois89@gmail.com>
2018-10-24 02:00:34 +0200

    up

--- src/main/java/pizzas/tp_poo/Livreur.java
+++ src/main/java/pizzas/tp_poo/Livreur.java
@@ -1,5 +1,12 @@
 package pizzas.tp_poo;
 
-public class Livreur {
+public class Livreur extends Client{
+	private String nom;
+	private String prenom;
+	private double solde;
+	public Livreur(int id, String nom, String prenom, double solde) {
+		super(id, nom, prenom, solde);
+		// TODO Auto-generated constructor stub
+	}
 
 }
